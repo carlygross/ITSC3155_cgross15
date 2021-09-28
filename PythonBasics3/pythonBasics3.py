7bcc46ac9483c81b87468dfb4043615b0dc1fb0a
cgross15 <cgross15@uncc.edu>
2021-09-28 12:49:07 -0400

    PythonBasics 3 - add ed logic to multiple_words function

--- PythonBasics3/pythonBasics3.py
+++ PythonBasics3/pythonBasics3.py
@@ -27,11 +27,11 @@ def starts_with_non_number(s):
 # Note: Be sure to use RegEx and that it works for any whitespace character 
 # with at least one character on either side!
 def multiple_words(s):
-  #RE_boundary = r"\b."
-  #if re.search(RE_boundary, s):
-    #return True
-
-  return
+  RE_boundary = r"\w\s\w"
+  if re.search(RE_boundary, s):
+    return True
+  else:
+    return False
 
 
 # Part C. reserved_us_tld
